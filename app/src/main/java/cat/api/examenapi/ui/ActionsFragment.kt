e334979a66776822a97c2ae969bac2d971dc8753
mcfl-uri <lamelasoriol@gmail.com>
2022-04-06 18:03:00 +0200

    get nom cicle

--- app/src/main/java/cat/api/examenapi/ui/ActionsFragment.kt
+++ app/src/main/java/cat/api/examenapi/ui/ActionsFragment.kt
@@ -1,22 +1,67 @@
 package cat.api.examenapi.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import cat.api.examenapi.R
-import cat.api.examenapi.databinding.FragmentAcionsBinding
+import cat.api.examenapi.api.ApiInterface
+import cat.api.examenapi.api.models.Cicle
+import cat.api.examenapi.databinding.FragmentActionsBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ActionsFragment : Fragment() {
 
-    lateinit var binding: FragmentAcionsBinding
+    lateinit var binding: FragmentActionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acions, container, false)
+        val binding = DataBindingUtil.inflate<FragmentActionsBinding>(
+            inflater,
+            R.layout.fragment_actions, container, false
+        )
+
+        binding.getCicleButton.setOnClickListener {
+            if (binding.getCicleInput.text.toString().isNotBlank())
+                getCicleById(binding.getCicleInput.text.toString().toInt())
+        }
+
+        return binding.root
     }
+
+    fun getCicleById(id: Int) {
+        val call = ApiInterface.create().getCicleById(id)
+
+        call.enqueue(object : Callback<Cicle> {
+
+            override fun onResponse(call: Call<Cicle>, response: Response<Cicle>) {
+                if (response?.body() != null) {
+                    var tmp: Cicle = response.body()!!
+                    view?.findViewById<TextView>(R.id.getNomCicle)?.setText(tmp.nomCicle)
+                }
+            }
+
+            override fun onFailure(call: Call<Cicle>, t: Throwable) {
+                showAlert()
+            }
+        })
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("Error")
+        builder.setMessage("Error al recuperar les dades")
+        builder.setPositiveButton("Tanca", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }
